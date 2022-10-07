d0ae05de116babc92319cf8ec4f4a619b00cfbb4
hortty <gabrielhortmann@gmail.com>
2022-10-06 23:25:28 -0300

    changes navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { BreakpointObserver } from "@angular/cdk/layout";
+import { MatSidenav } from '@angular/material/sidenav';
+
+export interface PeriodicElement {
+  id: number;
+  nome: string;
+  descricao: string;
+  preco: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'projeto_navbar';
+export class AppComponent implements AfterViewInit {
+
+    title = 'NewsApp';
+    @ViewChild(MatSidenav) sideNav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver, private cdr: ChangeDetectorRef) {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true;
+    this.observer.observe(['(max-width: 787px)']).subscribe((res)=> {
+        if(res?.matches) {
+            this.sideNav.mode = "over";
+            this.sideNav.close();
+        }else {
+            this.sideNav.mode = "side";
+            this.sideNav.open();
+        }
+    });
+    this.cdr.detectChanges();
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatDividerModule} from "@angular/material/divider"
+import {MatIconModule} from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,7 +15,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
